713a62524c8d3148ff1a4cac43115932c4c38a60
mthehero02 <johnmichaelraqueno@gmail.com>
2025-04-08 23:10:59 +0800

    test

--- lab02_raqueno.c
+++ lab02_raqueno.c
@@ -3,7 +3,7 @@
  * Section: CD-1L
  * Program Description: Simulation of Shortest Remaining Processing Time (SRPT) algorithm
  * Note: process.txt is patterned to the reference given in Slack
- * asdaasdsafgasdsadasdsa test
+ * asdaasdsafgasdsadasdsa test test2
  */
 
 #include <stdio.h>
